61e8cb3c1d1ff18b6d5686f53831938dbc50b5be
Jonathas Martins da Rocha <84166332+JhonAI13@users.noreply.github.com>
2024-09-02 00:48:22 -0300

    jupyter

--- teste.py
+++ teste.py
@@ -1,2 +1,19 @@
-lista = [[9,6,7,7],[6,5,4]]
-print(lista[1;0])
+from spellchecker import SpellChecker
+
+def corrigir_palavra(palavra):
+    # Cria um objeto SpellChecker
+    spell = SpellChecker()
+    
+    # Corrige a palavra
+    palavra_corrigida = spell.candidates(palavra)
+    if palavra_corrigida:
+        # Retorna a palavra mais provável (ou a primeira se houver várias)
+        return spell.candidates(palavra).pop()
+    else:
+        return palavra
+
+# Testa a função com uma palavra errada
+palavra_errada = 'gigima'
+palavra_corrigida = corrigir_palavra(palavra_errada)
+print(f'Palavra original: {palavra_errada}')
+print(f'Palavra corrigida: {palavra_corrigida}')
